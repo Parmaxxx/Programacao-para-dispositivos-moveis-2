c39c9b6aa70a2515551dfb7054581fde4dd03881
Ricardo FATEC <ric_msouza@hotmail.com>
2025-03-10 10:26:11 -0300

    Aula 03 - Parte 01 sobre StyleSheet

---
+++ Aula03/aula03/src/Aula03-pt01/Pagina1.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Text } from "react-native"
+import Estilo from "../Estilo"
+
+function Pagina1(){
+    return <Text style={Estilo.FontGrande}>Centro Paula Souza </Text>
+}
+export default Pagina1
---
+++ Aula03/aula03/src/Aula03-pt01/Pagina2.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import {Text} from 'react-native'
+import Estilo from "../Estilo"
+
+function Pagina2(){
+    return <Text style={Estilo.FontMedia}> Desenvolvimento de Software Multiplataforma</Text>
+}
+export default Pagina2
