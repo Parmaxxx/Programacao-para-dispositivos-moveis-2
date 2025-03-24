c04051d138591296be4c6753e0771608af1b4802
RicardoFATEC <ric_msouza@hotmail.com>
2025-03-24 09:36:07 -0300

    Aula 05 Utilizando UseState Atrelado ao useEffect

---
+++ Aula05/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Contador from '../src/Contador';
+import Contador2 from "@/src/Contador2";
+import Estilo from '../src/Estilo';
+import Contador3 from "@/src/Contador3";
+import Exemplo4 from "@/src/Exemplo4";
+import MudaCor from "../src/Exemplo5"
+
+export default () => (
+    <View style={Estilo.App}>
+        <MudaCor />
+    </View>
+);
+
